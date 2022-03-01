39b027f521c2bf3bf124a031bd0e264fbeb7d8f1
Zachary A. King <zaking17@gmail.com>
2022-03-01 08:30:51 -0800

    some stuff

---
+++ brain-cli/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='brain-cli',
+      version='0.1.0',
+      description='Brainshare CLI',
+      url='https://brainshare.io',
+      packages=find_packages(),
+      install_requires=['typer>=0.4.0,<0.5'],
+     )
