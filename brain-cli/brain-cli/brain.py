f9cc74c4a69f47f225abfc25dc23b7694478bd95
Zachary King <zaking17@gmail.com>
2022-03-01 13:25:45 -0800

    a

--- brain-cli/brain-cli/brain.py
+++ brain-cli/brain-cli/brain.py
@@ -1,9 +1,12 @@
 import typer
 
 
-
-def main(name: str):
+def main(name: str) -> None:
     typer.echo(f"Hello {name}")
+
+
+def eject() -> None:
+    typer.echo("Not implemented")
 
 
 if __name__ == "__main__":
--- brain-cli/setup.py
+++ brain-cli/setup.py
@@ -2,10 +2,12 @@
 
 from setuptools import setup, find_packages
 
-setup(name='brain-cli',
-      version='0.1.0',
-      description='Brainshare CLI',
-      url='https://brainshare.io',
-      packages=find_packages(),
-      install_requires=['typer>=0.4.0,<0.5'],
-     )
+setup(
+    name="brain-cli",
+    version="0.1.0",
+    description="Brainshare CLI",
+    url="https://brainshare.io",
+    packages=find_packages(),
+    install_requires=["typer>=0.4.0,<0.5"],
+    extras_require={"dev": ["mypy", "black", "types-setuptools"]},
+)
