d3dd1429548bdd87e5c9a37bb5c8a2c0786dedef
Zachary King <zaking17@gmail.com>
2022-03-01 14:32:00 -0800

    h

--- brain-cli/brain_cli/main.py
+++ brain-cli/brain_cli/main.py
@@ -1,6 +1,25 @@
 import typer
+import yaml
 
 app = typer.Typer()
+
+defaults = {"build-directory": ".brain-build"}
+required = ["name"]
+
+
+def load_config() -> None:
+    # get current directory
+    pass
+
+
+def get_from_config(key: str) -> str:
+    if key in brainshare_yml:
+        return brainshare_yml[key]
+    else:
+        return defaults[key]
+
+
+build_directory = get_from_config("build-directory")
 
 
 @app.command()
@@ -11,3 +30,9 @@ def main(name: str) -> None:
 @app.command()
 def eject() -> None:
     typer.echo("Not implemented")
+
+
+@app.command()
+def up() -> None:
+    sys.call("cd $BUILD_PATH")
+    sys.call("docker-compose up")
