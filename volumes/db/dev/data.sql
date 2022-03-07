854beb04c46786824b9e494090d0d1123f48dc7e
Zachary King <zaking17@gmail.com>
2022-03-07 07:31:25 -0800

    a

--- volumes/db/dev/data.sql
+++ volumes/db/dev/data.sql
@@ -60,23 +60,23 @@ create policy "Users can upload their own files."
   on storage.objects for insert
   with check ( bucket_id = 'uploaded_files' AND auth.role() = 'authenticated' );
 
--- create table uploaded_files (
---     "id" uuid NOT NULL DEFAULT extensions.uuid_generate_v4(),
---     "file_name" text,
---     "owner" uuid,
---     "object_id" text,
---     CONSTRAINT "objects_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id"),
---     CONSTRAINT "objects_objectId_fkey" FOREIGN KEY ("object_id") REFERENCES "storage"."objects"("id")
--- )
--- alter table base enable row level security;
+create table uploaded_files (
+    "id" UUID NOT NULL DEFAULT extensions.uuid_generate_v4(),
+    "file_name" text,
+    "owner" uuid,
+    "object_id" text,
+    CONSTRAINT "objects_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id"),
+    CONSTRAINT "objects_objectId_fkey" FOREIGN KEY ("object_id") REFERENCES "storage"."objects"("id")
+)
+alter table base enable row level security;
 
--- create policy "Users can select their own uploads"
---   on uploaded_files for select
---   using ( auth.uid() = owner );
+create policy "Users can select their own uploads"
+  on uploaded_files for select
+  using ( auth.uid() = owner );
 
--- create policy "Users can insert their own uploads"
---   on uploaded_files for insert
---   with check ( auth.uid() = owner );
+create policy "Users can insert their own uploads"
+  on uploaded_files for insert
+  with check ( auth.uid() = owner );
 
 -- Set up user database
 
