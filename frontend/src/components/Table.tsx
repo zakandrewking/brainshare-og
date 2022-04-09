3870dc056aa69470af2f077bb933ae05e462becd
Zachary King <zaking17@gmail.com>
2022-04-09 15:58:47 -0700

    asdf

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useParams, Link } from 'react-router-dom'
-import { definitions } from '../schema/rest-api'
 import supabase from '../api/supabaseClient'
 import useSwr from 'swr'
-import { FaCaretLeft, FaFileExcel } from 'react-icons/fa'
+import { FaFileExcel } from 'react-icons/fa'
 
 import { Body, Button } from './Components'
+
+/*
+ * Replace underscores with capital letters and spaces
+ */
+function snakeCaseToText (text: string): string {
+  return text
+    .replace(/^[a-z]/, (match) => match.toUpperCase())
+    .replace(/_([a-z])/g, (_, match) => ` ${match.toUpperCase()}`)
+}
 
 export function TableView ({ tableName }: { tableName: string }) {
   const [page, limit] = [0, 10]
@@ -22,11 +30,26 @@ export function TableView ({ tableName }: { tableName: string }) {
   // TODO does this effect happen on each route event?
   if (error) return <span>error</span>
   if (!data) return <span>loading</span>
+
+  // filter key list
+  const keys = data[0]
+    ? Object.keys(data[0]).filter(
+      (k) =>
+        !['id', 'created_at', 'updated_at', 'owner', 'object_key'].includes(k)
+    )
+    : null
+
   return (
     <Body>
       <div className="flex flex-col" role="list">
-        <div className="p-1">
-          <div className="grow-1 pl-9 font-bold">File name</div>
+        <div className="grow-1 flex flex-row space-x-3 pl-10 py-2">
+          {keys
+            ? keys.map((key) => (
+                <div key={key} className="font-bold">
+                  {snakeCaseToText(key)}
+                </div>
+            ))
+            : `You don't have any ${snakeCaseToText(tableName)} yet`}
         </div>
         {data.map((datum) => (
           <Link
@@ -38,7 +61,7 @@ export function TableView ({ tableName }: { tableName: string }) {
             <div className="w-9">
               <FaFileExcel size="1.5em" />
             </div>
-            <div>{datum.name ?? ''}</div>
+            {keys && keys.map((key) => <div key={key}>{datum[key]}</div>)}
           </Link>
         ))}
       </div>
@@ -59,11 +82,10 @@ export function TableDetail ({ tableName }: { tableName: string }) {
   if (!data) return <span>loading</span>
   return (
     <div className="flex flex-row items-center space-x-6 pl-3">
-      <Link to="..">
-        <Button>
-          <FaCaretLeft size={20} />
-        </Button>
+      <Link to="./..">
+        <Button>{snakeCaseToText(tableName)}</Button>
       </Link>
+      <span className="text-2xl">/</span>
       <span className="text-2xl">{data.name ?? ''}</span>
     </div>
   )
